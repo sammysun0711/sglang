98edf8451f17a511f87a963f247c31d292dfc6c4
Xiake Sun <xisun@amd.com>
2026-03-06 17:08:50 +0800

    Adjust Quick AllReduce priority via Environment Variable (#2)
    * Adjust QuickReduce priority over CustomAllReduce via environment variable ROCM_QUICK_REDUCE_QUANTIZATION
    
    Signed-off-by: Xiake Sun <xiake.sun@amd.com>
    
    * Apply review comments
    
    Signed-off-by: Xiake Sun <xiake.sun@amd.com>
    
    * Update Evaluation, add text script
    
    Signed-off-by: Xiake Sun <xiake.sun@amd.com>
    
    * Update scripts
    
    Signed-off-by: Xiake Sun <xiake.sun@amd.com>
    
    ---------
    
    Signed-off-by: Xiake Sun <xiake.sun@amd.com>

---
+++ evaluation/launch_server.sh
@@ -0,0 +1,14 @@
+export HIP_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export SGLANG_USE_AITER=1
+export ROCM_QUICK_REDUCE_QUANTIZATION=INT4
+
+python -m sglang.launch_server \
+    --model-path /models/Qwen3.5-397B-A17B \
+    --port 9000 \
+    --tp-size 8 \
+    --mem-fraction-static 0.8 \
+    --context-length 262144 \
+    --reasoning-parser qwen3 \
+    --attention-backend triton \
+    --disable-radix-cache \
+    --cuda-graph-max-bs 64
